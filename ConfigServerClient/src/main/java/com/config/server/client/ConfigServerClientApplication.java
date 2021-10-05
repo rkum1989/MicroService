2cbe7a18bb5d5029240abac7bde6b6ed476868f7
Rituraj kumar <riturajk1989@gmail.com>
2021-10-05 16:12:34 +0100

    updated spring version

--- ConfigServerClient/src/main/java/com/config/server/client/ConfigServerClientApplication.java
+++ ConfigServerClient/src/main/java/com/config/server/client/ConfigServerClientApplication.java
@@ -1,12 +1,25 @@
 package com.config.server.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RefreshScope
+@RestController
 @SpringBootApplication
 public class ConfigServerClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerClientApplication.class, args);
+	}
+
+	@Value("${hello.message}")
+	private String message;
+
+	@RequestMapping("/message")
+	String getMessage() {
+		return this.message;
 	}
 }
